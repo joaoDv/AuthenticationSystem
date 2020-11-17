294801fe02692f12d4c0e19a101145ed4e67f16a
Joao Porfirio <joaoprf54@gmail.com>
2020-11-16 21:03:39 -0300

    New package controller and New Class FormCadastroController

---
+++ src/controller/FormCadastroController.java
@@ -0,0 +1,46 @@
+package controller;
+
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+import com.mysql.jdbc.Connection;
+
+import dao.Conexao;
+import dao.UsuarioDAO;
+import model.Usuario;
+import view.FormCadastroView;
+
+public class FormCadastroController {
+
+	private FormCadastroView view;
+
+	public FormCadastroController(FormCadastroView view) {
+		this.view = view;
+	}
+	
+	public void salvaUsuario() {
+		
+		
+		String usuario = view.getTextFieldUsuario().getText();
+		String senha = view.getPasswordFieldSenha().getText();
+		String tipoDeUsuario = view.getTextFieldTipoUsuario().getText();
+					
+		
+		Usuario usuarioXande = new Usuario(usuario, senha, tipoDeUsuario);
+		
+		
+		
+		try {
+			Connection conexao = new Conexao().getConnection();
+			UsuarioDAO usuarioDao = new UsuarioDAO(conexao);
+			usuarioDao.insert(usuarioXande);
+			
+			JOptionPane.showMessageDialog(null, "Usuario Salvo Com Sucesso");
+		} catch (SQLException e1) {
+
+			e1.printStackTrace();
+		}
+		
+	}
+}
